7299b96df3ed04d501ab61846cecdf4a6cd7f9f3
Tony Brown <tony@tonybrown.me>
2017-11-25 00:04:26 -0500

    create some test to see if they will pass

---
+++ src/index.test.js
@@ -0,0 +1,22 @@
+
+/*
+** USING DESCRIBE
+*/
+describe('Our first tests', () => {
+
+  it('should work', () => {
+    expect(true).toEqual(true);
+  });
+
+  it('should work again.', () => {
+    expect(true).toEqual(true);
+  });
+
+});
+
+/*
+** Basic test
+*/
+// it('should work', () => {
+//   expect(true).toEqual(true);
+// });
